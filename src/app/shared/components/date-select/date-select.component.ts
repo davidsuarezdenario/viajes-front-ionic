a78716abac65acd55c94f414a070de63b60b952a
David Suarez <david.suarez@denario.com.co>
2024-02-10 18:39:39 -0500

    feat: acept button in date search

--- src/app/shared/components/date-select/date-select.component.ts
+++ src/app/shared/components/date-select/date-select.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
-import { IonItem, IonLabel, IonDatetimeButton, IonModal, IonDatetime } from "@ionic/angular/standalone";
+import { ModalController } from "@ionic/angular";
+import { IonItem, IonLabel, IonDatetimeButton, IonModal, IonDatetime, IonButton } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-date-select',
   templateUrl: './date-select.component.html',
   styleUrls: ['./date-select.component.scss'],
   standalone: true,
-  imports: [IonDatetime, IonModal, IonDatetimeButton, IonLabel, IonItem, FormsModule, CommonModule]
+  imports: [IonButton, IonDatetime, IonModal, IonDatetimeButton, IonLabel, IonItem, FormsModule, CommonModule]
 })
 export class DateSelectComponent  implements OnInit {
   @Input() label: string = "";
   @Input() ref: string = "datetime";
 
-  constructor() {}
+  constructor(
+    public modalController: ModalController
+  ) {}
 
   ngOnInit() {}
 
