34e71d49a47dc3f7d6ccf6371ca13041d4960075
David Suarez <david.suarez@denario.com.co>
2024-02-12 13:11:27 -0500

    feat: home to standalone

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
 import { CommonModule } from "@angular/common";
+import { IonHeader, IonProgressBar, IonToolbar, IonGrid, IonRow, IonCol, IonContent, IonCard, IonText } from "@ionic/angular/standalone";
 import { HeaderMainComponent } from "../../shared/components/header-main/header-main.component";
 import { SearchMainComponent } from "../../shared/components/search-main/search-main.component";
 import { ApiService } from "../../shared/services/api/api.service";
@@ -14,7 +14,7 @@ import { BookingComponent } from "../../shared/components/booking/booking.compon
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonicModule, HeaderMainComponent, SearchMainComponent, AlertMainComponent, BookingComponent],
+  imports: [IonText, IonCard, IonContent, IonCol, IonRow, IonGrid, IonToolbar, IonProgressBar, IonHeader, CommonModule, FormsModule, HeaderMainComponent, SearchMainComponent, AlertMainComponent, BookingComponent, ],
 })
 export class HomePage implements OnInit {
 
