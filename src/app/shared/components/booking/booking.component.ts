4c1357057d7cf762bca76c0e9569de4cf6d92310
David Suarez <david.suarez@denario.com.co>
2024-02-21 18:24:55 -0500

    feat: more segments in results

--- src/app/shared/components/booking/booking.component.ts
+++ src/app/shared/components/booking/booking.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { IonCard, IonGrid, IonRow, IonCardHeader, IonCol, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton } from "@ionic/angular/standalone";
+import { IonCard, IonGrid, IonRow, IonCardHeader, IonCol, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonImg, IonText } from "@ionic/angular/standalone";
 import { DatePipe, CurrencyPipe } from '@angular/common';
-import { format, differenceInSeconds } from 'date-fns';
+import { format } from 'date-fns';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss'],
   standalone: true,
-  imports: [IonButton, IonCardContent, IonCardTitle, IonCardSubtitle, IonCol, IonCardHeader, IonRow, IonGrid, IonCard, DatePipe, CurrencyPipe],
+  imports: [IonText, IonImg, IonButton, IonCardContent, IonCardTitle, IonCardSubtitle, IonCol, IonCardHeader, IonRow, IonGrid, IonCard, DatePipe, CurrencyPipe],
   providers: [DatePipe, CurrencyPipe]
 })
 export class BookingComponent  implements OnInit {
   @Input() flight: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('flight: ',this.flight);
+  }
 
   formatDate(date: string) {
     return format(new Date(date), 'EEE dd/MM');
@@ -30,6 +33,63 @@ export class BookingComponent  implements OnInit {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     return `${hours}h ${minutes}m`;
+  }
+
+  getScales(type: string): number{
+    const returnType = type === 'outbound' ? 0 : 1;
+    return this.flight.route.filter((segment: any) => segment.return === returnType).length - 1;
+  }
+
+  getFirstDeparturelSegment(): string {
+    const segment = this.flight.route
+      .filter((segment:any) => segment.return === 1)
+      .sort((a:any, b:any) => new Date(a.local_departure).getTime() - new Date(b.local_departure).getTime());
+    if (segment.length === 0) {
+      return "";
+    }
+    const lastSegment = segment[0].local_departure;
+    const lastSegmentTime = format(new Date(lastSegment), 'EEE dd/MM');
+    console.log('lastSegment: ',lastSegmentTime);
+    return lastSegmentTime;
+  }
+  
+  getLastArrivalSegment(): string {
+    const segment = this.flight.route
+      .filter((segment:any) => segment.return === 1)
+      .sort((a:any, b:any) => new Date(a.local_arrival).getTime() - new Date(b.local_arrival).getTime());
+    if (segment.length === 0) {
+      return "";
+    }
+    const lastSegment = segment[segment.length - 1].local_arrival;
+    const lastSegmentTime = format(new Date(lastSegment), 'EEE dd/MM');
+    console.log('lastSegment: ',lastSegmentTime);
+    return lastSegmentTime;
+  }
+  
+  getFirstDeparturelSegmentTime(): string {
+    const segment = this.flight.route
+      .filter((segment:any) => segment.return === 1)
+      .sort((a:any, b:any) => new Date(a.local_departure).getTime() - new Date(b.local_departure).getTime());
+    if (segment.length === 0) {
+      return "";
+    }
+    const lastSegment = segment[0].local_departure;
+    const lastSegmentTime = format(new Date(lastSegment), 'HH:mm');
+    console.log('lastSegment: ',lastSegmentTime);
+    return lastSegmentTime;
+  }
+  
+  getLastArrivalSegmentTime(): string {
+    const segment = this.flight.route
+      .filter((segment:any) => segment.return === 1)
+      .sort((a:any, b:any) => new Date(a.local_arrival).getTime() - new Date(b.local_arrival).getTime());
+    if (segment.length === 0) {
+      return "";
+    }
+    const lastSegment = segment[segment.length - 1].local_arrival;
+    const lastSegmentTime = format(new Date(lastSegment), 'HH:mm');
+    console.log('lastSegment: ',lastSegmentTime);
+    return lastSegmentTime;
   }
 
 }
--- src/app/shared/services/glb/glb.service.ts
+++ src/app/shared/services/glb/glb.service.ts
@@ -44,7 +44,7 @@ export class GlbService {
 
   constructor() {
     this.initCityFrom();
-    /* this.testBookingResults(); */
+    this.testBookingResults();
   }
 
   normalizeString(str: string): string {
@@ -151,104 +151,106 @@ export class GlbService {
       "type": "airport"
     };
     const airportsSelectedFrom = JSON.parse(localStorage.getItem('airportsSelectedFrom') || '[]');
-    if(airportsSelectedFrom.length == 0) this.selectAirportFrom = selectAirportFromMock;
+    if (airportsSelectedFrom.length == 0) this.selectAirportFrom = selectAirportFromMock;
     else this.selectAirportFrom = airportsSelectedFrom[0];
     console.log('selectAirportFrom: ', this.selectAirportFrom);
-    if(this.selectAirportFrom.type == 'city') this.searchFrom = `${this.selectAirportFrom.name}-${this.selectAirportFrom.code}`;
-    if(this.selectAirportFrom.type == 'airport') this.searchFrom = `${this.selectAirportFrom.city.name}-${this.selectAirportFrom.code}`;
+    if (this.selectAirportFrom.type == 'city') this.searchFrom = `${this.selectAirportFrom.name}-${this.selectAirportFrom.code}`;
+    if (this.selectAirportFrom.type == 'airport') this.searchFrom = `${this.selectAirportFrom.city.name}-${this.selectAirportFrom.code}`;
     const airportsSelectedTo = JSON.parse(localStorage.getItem('airportsSelectedTo') || '[]');
-    if(airportsSelectedTo.length == 0) this.selectAirportTo = {};
+    if (airportsSelectedTo.length == 0) this.selectAirportTo = {};
     else this.selectAirportTo = airportsSelectedTo[0];
     console.log('selectAirportTo: ', this.selectAirportTo);
-    if(this.selectAirportTo.type == 'city') this.searchTo = `${this.selectAirportTo.name}-${this.selectAirportTo.code}`;
-    if(this.selectAirportTo.type == 'airport') this.searchTo = `${this.selectAirportTo.city.name}-${this.selectAirportTo.code}`;
+    if (this.selectAirportTo.type == 'city') this.searchTo = `${this.selectAirportTo.name}-${this.selectAirportTo.code}`;
+    if (this.selectAirportTo.type == 'airport') this.searchTo = `${this.selectAirportTo.city.name}-${this.selectAirportTo.code}`;
   }
 
   testBookingResults() {
+    this.firstSearch = false;
     this.bookingResults = [
       {
-        "id": "049b1b174d410000474b7e3d_0|1b17049b4d4300004576c1e8_0",
+        "id": "049b0f6b4d3f00003b1c25f0_0|0f6b0a7c4d3f000066215fb6_0|25c30a224d450000c115a577_0|0a22049b4d450000a420b7d3_0",
         "flyFrom": "BOG",
-        "flyTo": "SMR",
+        "flyTo": "ORY",
         "cityFrom": "Bogotá",
         "cityCodeFrom": "BOG",
-        "cityTo": "Santa Marta",
-        "cityCodeTo": "SMR",
+        "cityTo": "París",
+        "cityCodeTo": "PAR",
         "countryFrom": {
           "code": "CO",
           "name": "Colombia"
         },
         "countryTo": {
-          "code": "CO",
-          "name": "Colombia"
+          "code": "FR",
+          "name": "Francia"
         },
-        "local_departure": "2024-02-24T18:43:00.000Z",
-        "utc_departure": "2024-02-24T23:43:00.000Z",
-        "local_arrival": "2024-02-24T20:15:00.000Z",
-        "utc_arrival": "2024-02-25T01:15:00.000Z",
-        "nightsInDest": 2,
-        "quality": 180.502019,
-        "distance": 714.51,
+        "local_departure": "2024-02-22T11:00:00.000Z",
+        "utc_departure": "2024-02-22T16:00:00.000Z",
+        "local_arrival": "2024-02-23T11:40:00.000Z",
+        "utc_arrival": "2024-02-23T10:40:00.000Z",
+        "nightsInDest": 5,
+        "quality": 1504.631266,
+        "distance": 8638.03,
         "duration": {
-          "departure": 5520,
-          "return": 5340,
-          "total": 10860
+          "departure": 67200,
+          "return": 72000,
+          "total": 139200
         },
-        "price": 557705,
+        "price": 4966838,
         "conversion": {
-          "COP": 557705,
-          "EUR": 132.36880603787435
+          "COP": 4966838,
+          "EUR": 1171.2987657582971
         },
         "fare": {
-          "adults": 557705,
-          "children": 557705,
-          "infants": 557705
+          "adults": 4966838,
+          "children": 4966838,
+          "infants": 4966838
         },
         "price_dropdown": {
-          "base_fare": 487846.09979999997,
-          "fees": 69858.90082633319
+          "base_fare": 5038925.711,
+          "fees": -72087.710214
         },
         "bags_price": {
-          "1": 36.387,
-          "2": 94.97800000000001
+          "1": 323.388
         },
         "baglimit": {
           "hold_dimensions_sum": 158,
           "hold_height": 52,
           "hold_length": 78,
-          "hold_weight": 20,
+          "hold_weight": 15,
           "hold_width": 28,
-          "personal_item_height": 35,
+          "personal_item_height": 30,
           "personal_item_length": 40,
           "personal_item_weight": 4,
           "personal_item_width": 20
         },
         "availability": {
-          "seats": 9
+          "seats": 1
         },
         "airlines": [
-          "P5",
-          "LA"
+          "LA",
+          "BF",
+          "U2",
+          "AV"
         ],
         "route": [
           {
-            "id": "049b1b174d410000474b7e3d_0",
-            "combination_id": "049b1b174d410000474b7e3d",
+            "id": "049b0f6b4d3f00003b1c25f0_0",
+            "combination_id": "049b0f6b4d3f00003b1c25f0",
             "flyFrom": "BOG",
-            "flyTo": "SMR",
+            "flyTo": "MIA",
             "cityFrom": "Bogotá",
             "cityCodeFrom": "BOG",
-            "cityTo": "Santa Marta",
-            "cityCodeTo": "SMR",
-            "local_departure": "2024-02-24T18:43:00.000Z",
-            "utc_departure": "2024-02-24T23:43:00.000Z",
-            "local_arrival": "2024-02-24T20:15:00.000Z",
-            "utc_arrival": "2024-02-25T01:15:00.000Z",
-            "airline": "P5",
-            "flight_no": 7284,
-            "operating_carrier": "",
-            "operating_flight_no": "",
-            "fare_basis": "",
+            "cityTo": "Miami",
+            "cityCodeTo": "MIA",
+            "local_departure": "2024-02-22T11:00:00.000Z",
+            "utc_departure": "2024-02-22T16:00:00.000Z",
+            "local_arrival": "2024-02-22T14:55:00.000Z",
+            "utc_arrival": "2024-02-22T19:55:00.000Z",
+            "airline": "LA",
+            "flight_no": 4402,
+            "operating_carrier": "LA",
+            "operating_flight_no": "4402",
+            "fare_basis": "NLESLZ0K",
             "fare_category": "M",
             "fare_classes": "",
             "return": 0,
@@ -259,23 +261,50 @@ export class GlbService {
             "vehicle_type": "aircraft"
           },
           {
-            "id": "1b17049b4d4300004576c1e8_0",
-            "combination_id": "1b17049b4d4300004576c1e8",
-            "flyFrom": "SMR",
-            "flyTo": "BOG",
-            "cityFrom": "Santa Marta",
-            "cityCodeFrom": "SMR",
-            "cityTo": "Bogotá",
-            "cityCodeTo": "BOG",
-            "local_departure": "2024-02-26T23:15:00.000Z",
-            "utc_departure": "2024-02-27T04:15:00.000Z",
-            "local_arrival": "2024-02-27T00:44:00.000Z",
-            "utc_arrival": "2024-02-27T05:44:00.000Z",
-            "airline": "LA",
-            "flight_no": 4163,
-            "operating_carrier": "LA",
-            "operating_flight_no": "4163",
-            "fare_basis": "O00QP5ZB",
+            "id": "0f6b0a7c4d3f000066215fb6_0",
+            "combination_id": "0f6b0a7c4d3f000066215fb6",
+            "flyFrom": "MIA",
+            "flyTo": "ORY",
+            "cityFrom": "Miami",
+            "cityCodeFrom": "MIA",
+            "cityTo": "París",
+            "cityCodeTo": "PAR",
+            "local_departure": "2024-02-22T21:00:00.000Z",
+            "utc_departure": "2024-02-23T02:00:00.000Z",
+            "local_arrival": "2024-02-23T11:40:00.000Z",
+            "utc_arrival": "2024-02-23T10:40:00.000Z",
+            "airline": "BF",
+            "flight_no": 743,
+            "operating_carrier": "BF",
+            "operating_flight_no": "743",
+            "fare_basis": "HBCOWUS",
+            "fare_category": "M",
+            "fare_classes": "H",
+            "return": 0,
+            "bags_recheck_required": true,
+            "vi_connection": true,
+            "guarantee": true,
+            "equipment": null,
+            "vehicle_type": "aircraft"
+          },
+          {
+            "id": "25c30a224d450000c115a577_0",
+            "combination_id": "25c30a224d450000c115a577",
+            "flyFrom": "CDG",
+            "flyTo": "MAD",
+            "cityFrom": "París",
+            "cityCodeFrom": "PAR",
+            "cityTo": "Madrid",
+            "cityCodeTo": "MAD",
+            "local_departure": "2024-02-28T07:05:00.000Z",
+            "utc_departure": "2024-02-28T06:05:00.000Z",
+            "local_arrival": "2024-02-28T09:20:00.000Z",
+            "utc_arrival": "2024-02-28T08:20:00.000Z",
+            "airline": "U2",
+            "flight_no": 4581,
+            "operating_carrier": "EC",
+            "operating_flight_no": "",
+            "fare_basis": "",
             "fare_category": "M",
             "fare_classes": "",
             "return": 1,
@@ -284,16 +313,43 @@ export class GlbService {
             "guarantee": false,
             "equipment": null,
             "vehicle_type": "aircraft"
+          },
+          {
+            "id": "0a22049b4d450000a420b7d3_0",
+            "combination_id": "0a22049b4d450000a420b7d3",
+            "flyFrom": "MAD",
+            "flyTo": "BOG",
+            "cityFrom": "Madrid",
+            "cityCodeFrom": "MAD",
+            "cityTo": "Bogotá",
+            "cityCodeTo": "BOG",
+            "local_departure": "2024-02-28T16:40:00.000Z",
+            "utc_departure": "2024-02-28T15:40:00.000Z",
+            "local_arrival": "2024-02-28T21:05:00.000Z",
+            "utc_arrival": "2024-02-29T02:05:00.000Z",
+            "airline": "AV",
+            "flight_no": 11,
+            "operating_carrier": "AV",
+            "operating_flight_no": "11",
+            "fare_basis": "UZOB1BR9",
+            "fare_category": "M",
+            "fare_classes": "U",
+            "return": 1,
+            "bags_recheck_required": true,
+            "vi_connection": true,
+            "guarantee": true,
+            "equipment": null,
+            "vehicle_type": "aircraft"
           }
         ],
-        "booking_token": "GMf8VBChbUVRzPxZv0jwRpgwAgkXauXZio29qGuTHbrXRs8WyfkBW-sdzkOqRNgMPicjUPethUD8lss3PxbSCfAH88dY1GFt0ATBIT_DVCGoA4VPw_bmlRr6yhOGmReJyhCjcXUlqKYRbvKsgECF1G4e6TOlztF_XvsUQsgXfTP2X6Y-HWLZvHBAjLiswhsF_9YHKWnk4a1flSuXkpX9auY8A6L97d_PDB6-zL2qy3LB3pR6l9fitJiz_-tbifhrSy5c7ImqI9JTvUmLKrQIae5q-P9B0Lko_BmYfFBTQyS0S0orpj2rMm0zc39Uft0c7zfaOzdKUCPCQekJQfv_QwGO7GxU1QrN1x8hJ-jOGAeG_-Z5NP0bHy2pWq4pYn9D-8kF7PFFTSBmvKxnfBTDw11AmQkA5Hfs_fy0hgtv7L6L9vlaSn5nOL__YTSe4o6Y2iC2ZrcPMU0t-6xrahoSIO_6niXWpZlseP0HeehvJwaNvIffDlR5zjvfzzHC3zZeSnWSEonarAS6toFzgxPgwCbH9GKGOG7IdtwGqSumih9QzwFIvcu_F4xMt17lUJVoLWPMuQuEZ8VHcfD8d8E2RvKgEp2sURINyO1BrCgpoass04KO29wRw4f51mBnQHJYPa2PqApqADBe7qsOf5GMYimp97FnYKFSskI7C_-Ztzw8CpOZ1U0C3JQH81eCBlRSdGrFsbt88IGisxBDrhfkKWg==",
+        "booking_token": "G6VIZRP2TtpNss-L5tRtv7YbLbvvPH--hdRDu82YWFLrZ3jVYwtWshcbgHn85PkBJ7ivH9QBxESmMhp0-i0fUVhwcWyqDWjXjHMgyZpML3c-Y70jpW2BC4laNFjhDnYxFDB6VNHHpoR3vrWAw7jb_Df32C1qsZAnaJOaHxRWDDaMErmBzGYUtLWefg5CZHkEdqF5xMNurAIP76L4migOg_KwwBv6e9CsEItHwlELfqNkT5k8UcOj60ESYhABY8fcfHDS_n0yPZzdzGsrfRMBbSniohehy39AP_gwQe6VAa-vNM1aWEKPs6Go87PO8GcrxBjyBPQUpsLnqQ0SUe26YB2D5t22vG1EccWbphURfFSD-rljqmsiGh37f5UHDgySP9aQ0jj7uKMJFPSzB7io2krGKcMIi4-2Cj8dc9UBgWj9DrrRpg77YC7rqgabIT6tcmTxq62BcfOGitiEFyVNZ0Skes7eBeC0wZc6fBeqraaOS6wPbo5UFqT4ELiQSay2YalUfAS0mBHE2bM203BNT8zHBklfF_M6mnUOCpYo_3Fw5Om_mZQQHslKxX4JfMcYC28ZHlkCWIW_1lQrrcCcUqldzwCTJH2pqi05G6AEdSBP0AdaRVdW6gklfrEwPehT2PhIwYhAJbQwM0FPL_eSaF-yXFRK28GT3X8GubFJI-nHo437LUVQS-q_7UO114Af5kIjhKvvr9jbMMJa_yGP26a2riEOM3ivBwG_qpQgTXdV847b1ncGk5IAqekhrPv3GAiY4Dj3_csO_dwRfK31iq1Q8BqbbjyIQqEtLsw2dG4CQ1JIyHjfhhT4HG-3U_bzruLj1Plxli5t_AKzuOs37biT-XdUw1JuC5NXq8oy5wldbyDMAAMDU1JZtVTtOpArw",
         "facilitated_booking_available": true,
-        "pnr_count": 2,
+        "pnr_count": 4,
         "has_airport_change": false,
         "technical_stops": 0,
         "throw_away_ticketing": false,
         "hidden_city_ticketing": false,
-        "virtual_interlining": false
+        "virtual_interlining": true
       }
     ];
   }
